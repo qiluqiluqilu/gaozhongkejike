0b1f9645e52d60f222b48774aca4bf7edba508c9
qiluqiluqilu <42962394+qiluqiluqilu@users.noreply.github.com>
2018-09-10 12:15:24 +0800

    Update blink2.ino

--- lesson2/blink2.ino
+++ lesson2/blink2.ino
@@ -2,15 +2,15 @@
 
 int a = 13;//定义全局变量LED_BUILTIN为整数13,13可以有很多含义，在不同的函数下13代表不同的含义。
 
-// the setup function runs only the first time
+// setup函数只运行最开始的一次
 void setup() {
-  pinMode(a, OUTPUT);  // initialize digital pin a as an output.在pinMode下13是指arduino第13号引脚，在UNO中，第13号引脚已经接到了板载LED灯，无须外置灯泡。
+  pinMode(a, OUTPUT);  //定义a引脚（13号引脚）为输出模式。
 }
 
-// the loop function runs over and over again forever
+// loop函数会一直循环运行
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(a, HIGH);   // 让a引脚处于高电平（有电），“数字写”的含义为数字输出（最高电压5v或最低电压0v）。
+  delay(1000);                       // 等待1000毫秒
+  digitalWrite(a, LOW);    // 让a引脚处于低电平（没电）。
+  delay(1000);                       // 
 }
